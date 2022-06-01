715a5c406c757bbaca7cd1e5fc2310ddbac38bb3
Ramzi-dev611 <ramzi.latrous@insat.u-carthage.tn>
2022-06-01 17:45:22 +0100

    modifying the grid layout + adding image togle event handler

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,22 +5,27 @@ import challengeStyles from '../styles/Home.module.css'
 import type { ChallengesResponseData } from './api/challenges'
 
 interface ChallengesInterface {
-  path : string[]
   response: ChallengesResponseData
 }
 
 const Home: NextPage<ChallengesInterface> = ({ response }: {response: ChallengesResponseData}) => {
-  const [imagesToggleState, setImagesToggleState] = useState([0,0,0,0,0,0,0,0,0]);
+  const [imagesToggleState, setImagesToggleState] = useState([false, false, false, false, false, false, false, false, false]);
   
   const sendResponse = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
-    // console.log(props.challenges)
-    console.log("response sent");
   }
 
-  const toggleImage = (event: React.MouseEvent<HTMLDivElement>) => {
-    
+  const toggleImage = (target: HTMLElement, index: number) => {
+    imagesToggleState[index] = ! imagesToggleState[index];
+    if(imagesToggleState[index]){
+      target.classList.add(challengeStyles.toggeledImage);
+      console.log(target.classList)
+    } else {
+      target.classList.remove(challengeStyles.toggeledImage)
+      console.log(target.classList)
+    }
   }
+
 
   const { statement, path } = response;
   return (
@@ -33,46 +38,13 @@ const Home: NextPage<ChallengesInterface> = ({ response }: {response: Challenges
         <div className="row">
           { path.map((element, index) => 
            { return (
-             <div className={challengeStyles.SelectableElement+" col-4"} key={index}>
-            <Image src={element} width='150px' height='150px' alt={statement}/>
+             <div className={`${challengeStyles.SelectableElement} col-4`} onClick={(event) => {event.preventDefault(); toggleImage(event.target as HTMLElement, index)}} key={index}>
+            <Image src={element} width='150px' height='150px' className='cursor-pointer' alt={statement}/>
           </div>
            )}) 
         }
         </div>
-        {/* <div className="row">
-          <div className={challengeStyles.SelectableElement+" col"}>
-            <Image src='/images/cat2.jpg' width='150px' height='150px' alt='cat image'/>
-          </div>
-          <div className={challengeStyles.SelectableElement+" col"}>
-            <Image src='/images/cat3.jpg' width='150px' height='150px' alt='cat image'/>
-          </div>
-          <div className={challengeStyles.SelectableElement+" col"}>
-            <Image src='/images/cat2.jpg' width='150px' height='150px' alt='cat image'/>
-          </div>
-        </div>
-        <div className="row">
-          <div className={challengeStyles.SelectableElement+" col"}>
-            <Image src='/images/cat.jpg' width='150px' height='150px' alt='cat image'/>
-          </div>
-          <div className={challengeStyles.SelectableElement+" col"}>
-            <Image src='/images/cat2.jpg' width='150px' height='150px' alt='cat image'/>
-          </div>
-          <div className={challengeStyles.SelectableElement+" col"}>
-            <Image src='/images/cat.jpg' width='150px' height='150px' alt='cat image'/>
-          </div>
-        </div>
-        <div className="row">
-          <div className={challengeStyles.SelectableElement+" col"}>
-            <Image src='/images/cat3.jpg' width='150px' height='150px' alt='cat image'/>
-          </div>
-          <div className={challengeStyles.SelectableElement+" col"}>
-            <Image src='/images/cat2.jpg' width='150px' height='150px' alt='cat image'/>
-          </div>
-          <div className={challengeStyles.SelectableElement+" col"}>
-            <Image src='/images/cat.jpg' width='150px' height='150px' alt='cat image'/>
-          </div>
-        </div> */}
-        <div className='mt-3 '>
+        <div className='mt-3'>
           <div className='d-flex justify-content-end mx-4'>
             <button onClick={sendResponse} className='btn btn-success px-4'>Verify</button>
           </div>
